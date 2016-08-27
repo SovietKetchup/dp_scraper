e25e47cece8b6deee9f746469aad6778220e1f0f
SovietKetchup <sam.comer+git@gmail.com>
2016-08-27 18:14:15 +0100

    Slightly functionalized

--- dp_scraper.rb
+++ dp_scraper.rb
@@ -1,6 +1,6 @@
 # Scrape posts from /r/dailyprogrammer
 # SovietKetchup
-# v0.2.0
+# v0.3.0
 
 require 'net/http'
 require 'json'
@@ -18,32 +18,30 @@ def get url
 end
 
 
+stages = []
+def write_stages s
+  File.open("js/raw_json.json", 'w')           {|f| f.write(s[0]) }
+  File.open("js/raw_data.json", 'w')           {|f| f.write(s[1]) }
+  File.open("js/pretty_data.txt", 'w')         {|f| f.write(s[2]) }
+  File.open("js/raw_posts.json", 'w')          {|f| f.write(s[3]) }
+  File.open("js/pretty_posts.txt", 'w')        {|f| f.write(s[4]) }
+  File.open("js/raw_posts_simple.json", 'w')   {|f| f.write(s[5]) }
+  File.open("js/pretty_posts_simple.txt", 'w') {|f| f.write(s[6]) }
+end
+
 raw_json = get("https://www.reddit.com/r/dailyprogrammer.json").body
-File.open("js/raw_json.json", 'w') {|f| f.write(raw_json) }
-
 raw_data = JSON.parse(raw_json)
-File.open("js/raw_data.json", 'w') {|f| f.write(raw_data) }
-
 pretty_data = JSON.pretty_generate(raw_data)
-File.open("js/pretty_data.txt", 'w') {|f| f.write(pretty_data) }
-
 raw_posts = (raw_data["data"]["children"])
 raw_posts.delete_at(0)
-File.open("js/raw_posts.json", 'w') {|f| f.write(raw_posts) }
-
 pretty_posts = JSON.pretty_generate(raw_posts)
-File.open("js/pretty_posts.txt", 'w') {|f| f.write(pretty_posts) }
 
 posts_arr = eval(raw_posts.inspect)
-
 c = 0
 raw_posts_simple = []
-
 posts_arr.each do |post|
-
   post = post["data"]
   po = Hash.new
-
   # Post data
   po[:title] = post["title"]
   po[:url] = post["url"]
@@ -51,13 +49,18 @@ posts_arr.each do |post|
   po[:score] = post["score"]
   po[:comments] = post["num_comments"]
   po[:description] = post["selftext"]
-
   raw_posts_simple[c] = po
-
   c += 1
 end
 
-File.open("js/raw_posts_simple.json", 'w') {|f| f.write(raw_posts_simple) }
-
 pretty_posts_simple = JSON.pretty_generate(raw_posts_simple)
-File.open("js/pretty_posts_simple.txt", 'w') {|f| f.write(pretty_posts_simple) }
+
+# Print each stage to files
+# stages = [raw_json]
+# stages << raw_data
+# stages << pretty_data
+# stages << raw_posts
+# stages << pretty_posts
+# stages << raw_posts_simplified
+# stages << pretty_posts_simplified
+# write_stages(stages)
